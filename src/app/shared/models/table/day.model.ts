d1b64986ebe321df4e3e7d379f2e341e0631d810
Артем Баранов <temoncher73@gmail.com>
2020-04-15 22:15:39 +0000

    Implemented player sliders and angular animation

--- src/app/shared/models/table/day.model.ts
+++ src/app/shared/models/table/day.model.ts
@@ -1,12 +1,22 @@
-import { Daytime } from './daytime.model';
-import { Nighttime } from './nighttime.model';
-
 export class Day {
-  nighttime: Nighttime;
-  daytime: Daytime;
+  kickedPlayer: string;
+  // Night
+  murderedPlayer: string; // murdered player id
+  donCheck: string; // id of player checked by Don
+  sheriffCheck: string; // id of player checked by Sheriff
 
-  constructor() {
-    this.nighttime = new Nighttime();
-    this.daytime = new Daytime();
+  // Day
+  timers: Map<string, number>; // <playerId, timeLeft>
+  proposedPlayers: string[]; // playerIds
+  votes: Map<string, string[]>[]; // <candidatePlayerId, votePlayerId[]>, multiple votes can occur on ties
+
+  constructor(day?: any) {
+    this.timers = day?.timers || new Map<string, number>();
+    this.proposedPlayers = day?.proposedPlayers || [];
+    this.votes = day?.votes || [];
+    this.kickedPlayer = day?.kickedPlayer;
+    this.murderedPlayer = day?.murderedPlayer;
+    this.donCheck = day?.donCheck;
+    this.sheriffCheck = day?.sheriffCheck;
   }
 }
--- src/app/shared/models/table/nighttime.model.ts
+++
@@ -1,5 +0,0 @@
-export class Nighttime {
-  murdered: string; // murdered player id
-  donCheck: string; // id of player checked by Don
-  sheriffCheck: string; // id of player checked by Sheriff
-}
