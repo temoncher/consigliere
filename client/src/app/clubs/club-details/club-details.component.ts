ea077bc8737a60997e13f9ddf9b82b3c04251031
temoncher <temoncher@yandex.ru>
2020-10-17 23:26:51 +0300

    fix some todos

--- client/src/app/clubs/club-details/club-details.component.ts
+++ client/src/app/clubs/club-details/club-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import { switchMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 import {
   ClubDetailsPageGQL,
@@ -19,7 +20,8 @@ import { consigliereLogo } from '@/shared/constants/avatars';
   templateUrl: './club-details.component.html',
   styleUrls: ['./club-details.component.scss'],
 })
-export class ClubDetailsComponent implements OnInit {
+export class ClubDetailsComponent implements OnInit, OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
   club: ClubDetailsPageQuery['club'];
   loading = true;
   statusLoading = true;
@@ -44,9 +46,9 @@ export class ClubDetailsComponent implements OnInit {
     private leaveClubGQL: LeaveClubGQL,
     public alertController: AlertController,
   ) {
-    // TODO: ubsubscribe
     // TODO: implement club not found page
     this.activateRoute.params.pipe(
+      takeUntil(this.destroy),
       switchMap(({ clubId }) => this.clubDetailsPageGQL.watch({ id: clubId }).valueChanges),
     ).subscribe(({ data, loading }) => {
       this.club = data.club;
@@ -56,6 +58,11 @@ export class ClubDetailsComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.unsubscribe();
+  }
 
   leaveClub() {
     const clubId = this.club.id;
