5033c61fe0d8ba64f342ab7b6a1c17ed184e9540
temoncher <temoncher@yandex.ru>
2020-10-18 04:22:30 +0300

    fix games index

--- server/src/firestore-hooks/games.hooks.ts
+++ server/src/firestore-hooks/games.hooks.ts
@@ -9,7 +9,7 @@ export const createGameByParticipantIndex = functions.firestore.document(`${Coll
     const gameData = gameSnapshot.data();
     const objectID = gameSnapshot.id;
 
-    return AlgoliaSingleton.client.clubsIndex.saveObject({ ...gameData, objectID });
+    return AlgoliaSingleton.client.gamesByParticipantIndex.saveObject({ ...gameData, objectID });
   });
 
 export const updateGameByParticipantIndex = functions.firestore.document(`${CollectionName.GAMES}/{gameId}`)
@@ -17,8 +17,8 @@ export const updateGameByParticipantIndex = functions.firestore.document(`${Coll
     const gameData = gameSnapshot.data();
     const objectID = gameSnapshot.id;
 
-    return AlgoliaSingleton.client.clubsIndex.saveObject({ ...gameData, objectID });
+    return AlgoliaSingleton.client.gamesByParticipantIndex.saveObject({ ...gameData, objectID });
   });
 
 export const deleteGameByParticipantIndex = functions.firestore.document(`${CollectionName.GAMES}/{gameId}`)
-  .onDelete((gameSnapshot) => AlgoliaSingleton.client.clubsIndex.deleteObject(gameSnapshot.id));
+  .onDelete((gameSnapshot) => AlgoliaSingleton.client.gamesByParticipantIndex.deleteObject(gameSnapshot.id));
